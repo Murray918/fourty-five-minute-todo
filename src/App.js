abb63391e1ff61b66ce85d1d2849505c7ebdad74
Andrew Murray <darbyguitars@gmail.com>
2019-09-30 16:33:17 -0500

    create and delete for the todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		todos: ['First Item', 'Second Item'],
+		inputValue: ''
+	}
+	// the only thing this should handle is actually the todo to state
+	handleCreateTodo = todo => {
+		this.setState({
+			todos: [todo, ...this.state.todos]
+		})
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		this.handleCreateTodo(this.state.inputValue)
+		this.setState({
+			inputValue: ''
+		})
+	}
+
+	handleOnChange = event => {
+		this.setState({
+			inputValue: event.target.value
+		})
+	}
+
+	handleDelete = id => {
+		// copy state
+		let currentState = this.state.todos
+		let newState = currentState.filter(item => currentState[id] !== item)
+		this.setState({
+			todos: newState
+		})
+	}
+
+	render() {
+		const renderedTodos = this.state.todos.map((item, index) => {
+			return (
+				<li key={index} className="todo">
+					{item}
+					<button onClick={() => this.handleDelete(index)}>delete</button>
+				</li>
+			)
+		})
+		return (
+			<div className="App">
+				<ul className="todos">{renderedTodos}</ul>
+				<form className="add-form" onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						onChange={this.handleOnChange}
+						value={this.state.inputValue}
+					/>
+					<input type="submit" value="add" />
+				</form>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
